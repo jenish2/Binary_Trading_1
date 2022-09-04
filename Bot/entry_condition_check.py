af4d6dacbfa734ceebc5d1a5758e402562e880ab
Jenish Dholariya <dholariyajenish.ddjj@gmail.com>
2022-09-05 00:08:28 +0530

    bot : index access error resolved

--- Bot/entry_condition_check.py
+++ Bot/entry_condition_check.py
@@ -10,13 +10,13 @@ class EntryConditionCheck:
                                                                    timeperiod=config['bollinger_band']['length'],
                                                                    nbdevup=config['bollinger_band'][
                                                                        'standard_deviation'])
-        if df['close'].iat(-1) >= (df['BBANDS_U'].iat(-1) * (1 + (config['x_%_value'] / 100))):
+        if df['close'].iat[-1] >= (df['BBANDS_U'].iat[-1] * (1 + (config['x_%_value'] / 100))):
             if not config['reverse_condition']:
                 return True, 'put'
             else:
                 return True, 'call'
 
-        if df['close'].iat(-1) <= (df['BBANDS_L'].iat(-1) * (1 - (config['y_%_value'] / 100))):
+        if df['close'].iat[-1] <= (df['BBANDS_L'].iat[-1] * (1 - (config['y_%_value'] / 100))):
             if not config['reverse_condition']:
                 return True, 'call'
             else:
