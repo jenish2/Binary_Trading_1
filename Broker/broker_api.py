3c52997d124738478962ddc7dec352d3e651425c
Jenish Dholariya <dholariyajenish.ddjj@gmail.com>
2022-09-04 13:33:19 +0530

    broker_api : Login and Candle funtion added

--- Broker/broker_api.py
+++ Broker/broker_api.py
@@ -1,1 +1,67 @@
-#Author : Jenish Dholariya
+# Author : Jenish Dholariya
+import pandas as pd
+from iqoptionapi.stable_api import IQ_Option
+
+
+class BrokerIqOption:
+    def __init__(self, credentials: dict, balance_type):
+        self.EMAIL = credentials['user_email']
+        self.PASSWORD = credentials['password']
+        self.BALANCE_TYPE = balance_type  # PRACTICE / REAL / TOURNAMENT
+
+    def connect(self):
+        iq = IQ_Option(self.EMAIL, self.PASSWORD)
+        check, reason = iq.connect()
+        if check:
+            print("\nSuccessfully logged in account!!\n")
+            self.IQ = iq
+            self.IQ.change_balance(self.BALANCE_TYPE)
+        else:
+            while True:
+                if not iq.check_connect():
+                    print("Trying reconnect!")
+                    check, reason = iq.connect()
+                    if check:
+                        print("\nSuccessfully logged in account!!\n")
+                        return iq()
+                    else:
+                        print("\nFailed login due to  below reason:-")
+                        print(reason)
+                        print()
+                else:
+                    self.IQ = iq
+                    self.IQ.change_balance(self.BALANCE_TYPE)
+
+    def get_candle_data(self, goal: str, timeframe_of_chart_in_minute: int):
+        timestamp = self.IQ.get_server_timestamp()
+        candle = []
+        for _ in range(6):
+            x = self.IQ.get_candles(goal, (timeframe_of_chart_in_minute * 60), 1000, timestamp)
+            timestamp = int(x[0]["from"]) - 1
+            candle += x
+
+        dataframe = pd.DataFrame(candle)
+        dataframe.sort_values(by=["from"], inplace=True, ascending=True)
+        dataframe.drop(dataframe.tail(1).index, inplace=True)
+        print("data fetched by api")
+        return dataframe[["from", "close", "min", "max", "volume"]]
+
+    def place_order(self):
+        self.IQ
+        pass
+
+    @property
+    def balance(self):
+        return self.IQ.get_balance()
+
+
+if __name__ == "__main__":
+    cred = {
+        "user_email": "rolimiw174@vpsrec.com",
+        "password": "Abc@123@Xyz"
+    }
+
+    biq = BrokerIqOption(credentials=cred, balance_type="PRACTICE")
+    biq.connect()
+    print(biq.get_candle_data(goal="EURUSD", timeframe_of_chart_in_minute=5))
+    print(biq.balance)
